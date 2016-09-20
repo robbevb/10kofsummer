84cb52d256aec324696e1cf3a661726ded9a1d86
robbevb <robbe@v-b.be>
2016-09-20 10:12:53 +0200

    init config.rb

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Set this to the root of your project when deployed:
+http_path = "../"
+css_dir = ""
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :compressed
+line_comments = false
